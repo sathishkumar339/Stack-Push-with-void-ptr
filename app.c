bf1d086860364e503cb72791f82e1fd01a2942b1
sathishkumar339 <40970281+sathishkumar339@users.noreply.github.com>
2018-12-19 07:24:38 +0530

    Add files via upload
    Here I used Dynamic memory allocation for storing the values.

---
+++ app.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include "stack.h"
+
+int main() {
+
+	int choice,num,nmax;
+	stack_t *sd;
+
+	printf("Enter no of items to Push : \t");
+	scanf("%d",&nmax);
+
+	if((nmax <= MIN_ITEMS) || (nmax >= MAX_ITEMS)) {
+		fprintf(stderr,"No of Items should be between %d .. %d \n",MIN_ITEMS,MAX_ITEMS);
+		main();
+	}
+
+	sd = open_stack(nmax, sizeof num);
+	if(sd == NULL) {
+		fprintf(stderr,"Failed to open stack\n");
+		exit(1);
+	}
+
+	while(1) {
+		help();
+		printf("Enter your choice :\t");	
+		scanf("%d",&choice);
+		
+		switch (choice) {
+		case PUSH_OP:
+			if(is_stack_full(sd)) {
+				printf("stack is Full\n");
+			}	else {
+				printf("Enter the Number to Push : \t");
+				scanf("%d",&num);
+				push(sd,&num);
+			}
+			break;
+
+		case POP_OP:
+			if(is_stack_empty(sd)) {
+				fprintf(stderr,"stack is empty\n");
+			} else {
+				pop(sd,&num);
+				printf("Popped num is : %d\n",num);
+			}
+			break;
+
+		case EXIT_OP:
+			printf("Bye\n");
+			exit(1);
+		
+		default :
+			printf("Invalid choice\n");
+			break;
+		}
+	}
+}	
+
+int help() {
+
+	printf(" Press 1 for PUSH_OP\n Press 2 for POP_OP\n press 3 for EXIT\n");
+
+}			
+			
+				
---
+++ lib.c
@@ -0,0 +1,67 @@
+#include "stack.h"
+
+stack_t *open_stack(int nmax,int size)
+{
+	stack_t *s =NULL;
+	if((nmax <= MIN_ITEMS) || (nmax >= MAX_ITEMS)) {
+	fprintf(stderr,"nmax should be in range of %d .. %d\n",MIN_ITEMS,MAX_ITEMS);
+	}	
+	
+	if(size <= 0) {
+		fprintf(stderr,"size is should be greater than 0");
+		return NULL;
+	}
+
+	if((s = malloc(sizeof *s)) == NULL) {
+		fprintf(stderr,"Failed to allocate Memory for stack\n");
+		return NULL;
+	}
+	
+	s->start = malloc(nmax * size);
+	if(s->start == NULL) {
+		fprintf(stderr,"Failed to allocate Memory");
+		return NULL;
+	}
+
+	s->end = s->start + (nmax * size);
+	s->sp = s->start;
+	s->nitems = 0;
+	s->nmax = nmax;
+	s->size = size;
+
+	return s;
+}
+
+int push(stack_t *sd, void *data)
+{
+	if(is_stack_full(sd)) {
+		fprintf(stderr,"stack is full, no more to push\n");
+		return S_FULL;
+	}
+	memcpy(sd->sp,data,sd->size);
+	sd->sp += sd->size;
+	sd->nitems++;
+	return S_SUCCESS;
+}
+
+int pop(stack_t *sd, void *data)
+{
+	if(is_stack_empty(sd)) {
+		fprintf(stderr,"stack is empty,no items to pop\n");
+		return S_EMPTY;
+	}
+	
+	sd->sp -= sd->size;
+	memcpy(data,sd->sp,sd->size);
+	sd->nitems--;
+	return S_SUCCESS;
+}
+
+int close_stack(stack_t *sd)
+{
+	free(sd->start);
+	free(sd);
+}
+
+				
+
